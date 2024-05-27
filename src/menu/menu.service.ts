830ee077875eaea0e7830756fe82e9e99c87870f
Sebastian Contreras <sstfmscontreras@gmail.com>
2024-05-27 12:33:18 -0500

    transform object endpoint get menus of user

--- src/menu/menu.service.ts
+++ src/menu/menu.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { TransactionContext } from 'src/common/contexts/transaction.context';
+import { BaseService } from 'src/common/services/base.service';
 import { Menu } from 'src/generated/nestjs-dto/menu.entity';
 import { PermissionService } from 'src/permission/permission.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class MenuService {
+export class MenuService extends BaseService {
 
     constructor(
-        private readonly prismaService: PrismaService,
+        protected readonly prismaService: PrismaService,
         private readonly permissionService: PermissionService
-    ) { }
+    ) {
+        super(prismaService)
+    }
 
     // get list of menus of user
-    async getUserMenus(userId: number): Promise<Menu[]> {
-        const userPermissions = await this.permissionService.getUserPermissions(userId);
+    async getUserMenus(userId: number, txContext?: TransactionContext): Promise<Menu[]> {
+        const prisma = this.getPrismaClient(txContext);
 
-        return this.prismaService.menu.findMany({
+
+        const userPermissions = await this.permissionService.getUserPermissions(userId, txContext);
+
+        // this return (menu --> parent)
+        const menus = await prisma.menu.findMany({
             where: {
                 permissions: {
                     some: {
@@ -34,6 +42,37 @@ export class MenuService {
                 },
             },
         });
+
+        // build reverse result (parent --> childrens (submenus))
+        const result = [];
+
+        menus.forEach(menu => {
+            if (menu.parentId) {
+                const parent = {
+                    ...menu.parent,
+                    submenus: [],
+                }
+                menu.parent = null
+                parent.submenus.push(menu);
+
+                if (parent.parentId) {
+                    const grandParent = {
+                        ...parent.parent,
+                        submenus: [],
+                    }
+                    parent.parent = null
+                    grandParent.submenus.push(parent)
+
+                    result.push(grandParent);
+                } else {
+                    result.push(parent);
+                }
+            } else {
+                result.push(menu)
+            }
+        });
+
+        return result;
     }
 
 }
